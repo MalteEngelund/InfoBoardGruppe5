832a065b64846cc69aac5d166218e2d0ed72b7e4
Marie-Pierre Hélène Lessard <male9@elev.techcollege.dk>
2025-11-10 20:26:20 +0100

    Basic layout structure
    Initial setup of app container with columns.

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/* Eksempel af kodning til appens container til forskellige sektioner (klassenavne er ikke endelige) */
+
+const infoboardRootByMariePierreLessard = document.getElementById("app");
+infoboardRoot.classList("infoboard-grid-by-Marie-Pierre-Lessard");
+
+const columnOneByMariePierreLessard = Div();
+columnOneByMariePierreLessard.classList("left-column-by-Marie-Pierre-Lessard");
+const columnTwoByMariePierreLessard = Div();
+columnTwoByMariePierreLessard.classList("central-column-by-Marie-Pierre-Lessard");
+const columnThreeByMariePierreLessard = Div();
+columnThreeByMariePierreLessard.classList("right-column-by-Marie-Pierre-Lessard");
+
+infoboardRoot.append(columnOneByMariePierreLessard, columnTwoByMariePierreLessard, columnThreeByMariePierreLessard);
+
+/* TO DO: 
+append sections to relevant column */
+
+
