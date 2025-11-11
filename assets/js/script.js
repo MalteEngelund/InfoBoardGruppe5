a8a5b2c70fdc5b58d2402403081f6dc3f1174e4b
Marie-Pierre Hélène Lessard <male9@elev.techcollege.dk>
2025-11-11 13:26:44 +0100

    Saving branch for 1st time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,46 @@
-/* Example of import that goes with the 2nd example below.
+/* Example of import that goes with the example below.
 import {busScheduleViewFunction} from "./view/organisms/busScheduleView.js";
 */
 
-/* Eksempel af kodning til appens container til forskellige sektioner (klassenavne er ikke endelige) */
-
+/* 1. generation 
 const infoboardRootByMariePierreLessard = document.getElementById("app");
-infoboardRoot.classList("infoboard-grid-by-Marie-Pierre-Lessard");
+*/
 
+/* 2. generation 
+//Bo said that we don't need a header since SEO doesn't matter.
+const globalHeaderByMariePierreLessard = document.createElement("header");
+const mainByMariePierreLessard = document.createElement("main");
+mainByMariePierreLessard.classList("top-level-grid-by-Marie-Pierre-Lessard");
+const globalFooterByMariePierreLessard = document.createElement("footer");
+globalFooterByMariePierreLessard.classList("global-footer-by-Marie-Pierre-Lessard");
+infoboardRootByMariePierreLessard.append(globalHeaderByMariePierreLessard, mainByMariePierreLessard, globalFooterByMariePierreLessard);
+*/
+
+/* 3. generation 
 const columnOneByMariePierreLessard = Div();
 columnOneByMariePierreLessard.classList("left-infoboard-column-by-Marie-Pierre-Lessard");
-
 const columnTwoByMariePierreLessard = Div();
 columnTwoByMariePierreLessard.classList("right-infoboard-column-by-Marie-Pierre-Lessard");
+
+
+//4. generation 
 const columnTwoLeftByMariePierreLessard = Div();
 columnTwoLeftByMariePierreLessard.classList("left-side-of-column-two-by-Marie-Pierre-Lessard");
 const columnTwoRightByMariePierreLessard = Div();
 columnTwoRightByMariePierreLessard.classList("right-side-of-column-two-by-Marie-Pierre-Lessard");
 columnTwoByMariePierreLessard.append(columnTwoLeftByMariePierreLessard, columnTwoRightByMariePierreLessard);
 
-infoboardRoot.append(columnOneByMariePierreLessard, columnTwoByMariePierreLessard);
+mainByMariePierreLessard.append(columnOneByMariePierreLessard, columnTwoByMariePierreLessard);
+*/
 
-/* TO DO: 
-append sections to relevant column 
 
-For eksempel:
+/* For eksempel: append sections to relevant column as follows
 
 const busScheduleByMalte = busScheduleViewFunction(argument1, argument2);
 busScheduleByMalte.classList("global-styling-for-coloured-lines-by-Marie-Pierre-Lessard span-element-styling-by-Malte");
 columnOneByMariePierreLessard.append(busScheduleByMalte(argument1, argument2));
 
 */
-
-/* TO DO 
-create obligatory top-level elements:
-- global header (contains invisible text or has min height)
-- global footer (contains TechCollege logo) */
 
 
 
