1294857f2292a1404f5bb07b868974df94976591
Marie-Pierre Hélène Lessard <male9@elev.techcollege.dk>
2025-11-11 06:32:55 +0100

    Refactor infoboard section container code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,11 @@ const infoboardRootByMariePierreLessard = document.getElementById("app");
 infoboardRoot.classList("infoboard-grid-by-Marie-Pierre-Lessard");
 
 const columnOneByMariePierreLessard = Div();
-columnOneByMariePierreLessard.classList("left-column-by-Marie-Pierre-Lessard");
+columnOneByMariePierreLessard.classList("left-infoboard-column-by-Marie-Pierre-Lessard");
 const columnTwoByMariePierreLessard = Div();
-columnTwoByMariePierreLessard.classList("central-column-by-Marie-Pierre-Lessard");
+columnTwoByMariePierreLessard.classList("central-infoboard-column-by-Marie-Pierre-Lessard");
 const columnThreeByMariePierreLessard = Div();
-columnThreeByMariePierreLessard.classList("right-column-by-Marie-Pierre-Lessard");
+columnThreeByMariePierreLessard.classList("right-infoboard-column-by-Marie-Pierre-Lessard");
 
 infoboardRoot.append(columnOneByMariePierreLessard, columnTwoByMariePierreLessard, columnThreeByMariePierreLessard);
 
@@ -17,11 +17,12 @@ append sections to relevant column
 
 For eksempel:
 
-const busScheduleByMalte = mainBusController();
+const busScheduleByMalte = busScheduleViewFunction();
 busScheduleByMalte.classList("global-styling-for-coloured-lines-by-Marie-Pierre-Lessard span-element-styling-by-Malte");
 columnOneByMariePierreLessard.append(busScheduleByMalte);
 
 */
+
 
 
 
