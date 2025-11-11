3b3a9d763b899843b7470341cf24c35172cc6664
MalteEngelund <malte.engelund@hotmail.com>
2025-11-11 13:39:35 +0100

    jack heeelp

---
+++ assets/js/controller/scheduleController.js
@@ -0,0 +1,10 @@
+/*
+  struktur
+"value": [
+    {
+      "Team": "ggr080125",
+      "StartDate": "2025-11-11T07:15:00+01:00",
+      "Subject": "Grafisk teknik.",
+      "Education": "Grafisk teknik.",
+      "Room": "N112b"
+    } */
